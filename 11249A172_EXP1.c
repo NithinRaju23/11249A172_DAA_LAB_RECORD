c91309d9daae3905945afb088e8c416b035067e8
NithinRaju23 <11249A172@kanchiuniv.ac.in>
2026-04-26 13:09:46 +0530

    Update 11249A172_EXP1.c

--- 11249A172_EXP1.c
+++ 11249A172_EXP1.c
@@ -1,3 +1,6 @@
+Aim:
+To implement the Tower of Hanoi problem using recursion in C and display the sequence of moves required to transfer disks from source to destination.
+program:
 #include <stdio.h>
 void hanoi(int n, char a, char b, char c) {
     if (n == 0) return;
